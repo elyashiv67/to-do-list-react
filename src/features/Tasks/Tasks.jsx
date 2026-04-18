22b4426fd258079a9498a27cf96fa9f3630c0442
elyashiv67 <elishivs5@gmail.com>
2026-04-18 16:43:07 +0300

    finished api for tasks

--- src/features/Tasks/Tasks.jsx
+++ src/features/Tasks/Tasks.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import {useState} from 'react';
 import './Tasks.css';
 import Task from "./Task/Task.jsx";
 import {useGetallTasks} from "./apiHooksTasks.js";
 import InputsTask from "../InputsForTask/InputsTask.jsx";
+import { IoIosAddCircleOutline } from "react-icons/io"; // for now, its good change when doing CSS
 
 function Tasks() {
+    const [isAddToggle, setIsAddToggle] = useState(false);
     const {isPending, isError, data, error}= useGetallTasks();
     if (isPending) return <p>Loading...</p>;
     if (isError) return <p>Error: {error.message}</p>;
-    console.log(data);
+
+
+    function handleAddToggle(){
+        setIsAddToggle(!isAddToggle);
+    }
     return (
         <div>
-            <InputsTask/>
+            <IoIosAddCircleOutline onClick={handleAddToggle}/>
+
+            {isAddToggle &&(
+                <div className={"addToggle"} >
+                    <InputsTask/>
+                </div>
+            )}
+
             <div className="tasks_wrapper">
                 {data.map((task) => {
-                    return <Task data={task}/>
+                    return <Task key={task.id} data={task}/>
                 })}
             </div>
 
