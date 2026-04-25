ad43f433009cf19ff640f1be78bc9e6821af434d
elyashiv67 <elishivs5@gmail.com>
2026-04-25 17:58:46 +0300

    added functionalty to tasks page now the current task open on the left side and can be closed by clicking outside the panel

--- src/features/Tasks/Tasks.jsx
+++ src/features/Tasks/Tasks.jsx
@@ -3,42 +3,69 @@ import './Tasks.css';
 import TaskDetails from "./TaskDetails/TaskDetails.jsx";
 import {useGetallTasks} from "./apiHooksTasks.js";
 import InputsTask from "./InputsForTask/InputsTask.jsx";
-import { IoIosAddCircleOutline } from "react-icons/io";
+import {IoIosAddCircleOutline} from "react-icons/io";
 import TaskRow from "./TaskRow/TaskRow.jsx"; // for now, its good change when doing CSS
 
 function Tasks() {
     const [isAddToggle, setIsAddToggle] = useState(false);
     const [selectedTask, setSelectedTask] = useState(null);
 
-    const {isPending, isError, data:tasks, error}= useGetallTasks();
+    const {isPending, isError, data: tasks, error} = useGetallTasks();
     if (isPending) return <p>Loading...</p>;
     if (isError) return <p>Error: {error.message}</p>;
 
 
-    function handleAddToggle(){
+    function handleAddToggle() {
         setIsAddToggle(!isAddToggle);
     }
+
     return (
         <div className={"main-tasks"}>
-            <IoIosAddCircleOutline onClick={handleAddToggle}/>
 
-            {isAddToggle &&(
-                <div className={"addToggle"} >
-                    <InputsTask/>
+            <div className={"layout-tasks"}>
+
+                <div className={`left-panel ${selectedTask ? 'open' : ''}`}>
+
+                    {selectedTask && (
+                        <>
+
+                            <div
+                                className="invisible-overlay"
+                                onClick={() => setSelectedTask(null)}
+                            />
+
+                            <div className="left-details-panel">
+                                <TaskDetails
+                                    key={selectedTask.id}
+                                    task={selectedTask}
+                                    onClose={() => setSelectedTask(null)}/>
+                            </div>
+
+                        </>
+                    )}
                 </div>
-            )}
 
-            <div className="tasks_wrapper">
-                {tasks.map((task) => {
-                    return <TaskRow key={task.id} task={task} onClick={()=>{setSelectedTask(task)}}/>
-                })}
+
+                <div className="tasks_wrapper">
+                    {tasks.map((task) => {
+                        return <TaskRow key={task.id} task={task} onClick={() => {
+                            setSelectedTask(task)
+                        }}/>
+                    })}
+
+                    <div className={"add-btn"}>
+                        <IoIosAddCircleOutline onClick={handleAddToggle}/>
+                        {isAddToggle && (
+                            <div className={"addToggle"}>
+                                <InputsTask/>
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+
             </div>
 
-            {selectedTask && (
-                <div className="left-details-panel">
-                    <TaskDetails task={selectedTask} onClose={() => setSelectedTask(null)} />
-                </div>
-            )}
 
         </div>
     );
