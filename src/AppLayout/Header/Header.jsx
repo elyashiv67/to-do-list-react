aebb7468eea3012d6c7ec5464cd770712377637d
elyashiv67 <elishivs5@gmail.com>
2026-05-07 11:55:30 +0300

    added log out functionality

--- src/AppLayout/Header/Header.jsx
+++ src/AppLayout/Header/Header.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './Header.css';
 import Nav from "../Nav/Nav.jsx";
+import { IoMdLogOut } from "react-icons/io";
+import Cookies from "js-cookie";
+import {useNavigate} from "react-router";
 
 function Header() {
+    const navigate = useNavigate();
+    function logout() {
+        if(confirm("Are you sure you want to logout?")){
+            Cookies.remove("jwt");
+            navigate("/Login");
+        }
+    }
     return (
         <header className="header">
             <div className="header-logo">To Do List REACT</div>
             <Nav/>
+            <span className={"log-out-btn"} onClick={logout}><IoMdLogOut/></span>
         </header>
     );
 }
